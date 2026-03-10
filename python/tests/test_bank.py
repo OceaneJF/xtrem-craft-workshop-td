df10cf7f2c2c912810eb3dbd404f9312e63d2b56
Anton Beziat <anton.beziat@etu.u-bordeaux.fr>
2026-03-10 09:48:03 +0100

    refactoring tests

--- python/tests/test_bank.py
+++ python/tests/test_bank.py
@@ -7,21 +7,31 @@ from python.src.missing_exchange_rate_error import MissingExchangeRateError
 
 
 class TestBank:
-    def test_convert_euro_to_usd_returns_float(self):
-        assert Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.USD) == 12
+    
+    bank : Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
+    
+    def test_should_convert_euro_to_usd_returns_float(self):
+        expected = 12
+        
+        converted = self.bank.convert(10, Currency.EUR, Currency.USD)
+        
+        assert converted == expected
 
-    def test_convert_euro_to_usd_returns_same_value(self):
-        assert Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.EUR) == 10
+    def test_should_convert_euro_to_usd_returns_same_value(self):
+        expected = 10
+        
+        converted = self.bank.convert(10, Currency.EUR, Currency.EUR)
+        assert converted == expected
 
-    def test_convert_with_missing_exchange_rate_throws_exception(self):
+    def test_should_convert_with_missing_exchange_rate_throws_exception(self):
         with pytest.raises(MissingExchangeRateError) as error:
-            Bank.create(Currency.EUR, Currency.USD, 1.2).convert(10, Currency.EUR, Currency.KRW)
+            self.bank.convert(10, Currency.EUR, Currency.KRW)
         
         assert str(error.value) == "EUR->KRW"
 
-    def test_convert_with_different_exchange_rate_returns_different_floats(self):
-        bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
-        assert bank.convert(10, Currency.EUR, Currency.USD) == 12
-
-        bank.addEchangeRate(Currency.EUR, Currency.USD, 1.3)
-        assert bank.convert(10, Currency.EUR, Currency.USD) == 13
+    def test_should_convert_with_different_exchange_rate_returns_different_floats(self):
+        converted1 = self.bank.convert(10, Currency.EUR, Currency.USD)
+        self.bank.addEchangeRate(Currency.EUR, Currency.USD, 1.3)
+        converted2 = self.bank.convert(10, Currency.EUR, Currency.USD)
+        assert converted1 != converted2
+        
