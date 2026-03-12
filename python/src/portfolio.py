560f9d36229d5884999c38fa6c04089791f4c814
Anh Tin NGUYEN <tinnguyenanh14@gmail.com>
2026-03-12 14:34:02 +0100

    more tests

--- python/src/portfolio.py
+++ python/src/portfolio.py
@@ -7,6 +7,8 @@ class Portfolio:
         self.amount_dict: dict[Currency, float] = {}
     
     def evaluate(self, bank: Bank, currency: Currency) -> float:
+        if not isinstance(currency, Currency):
+            raise TypeError("currency should be an instance of Currency")
         total = 0
         for c, amount in self.amount_dict.items():
             total = total + bank.convert(amount, c, currency)
@@ -16,4 +18,6 @@ class Portfolio:
     def deposit(self, amount: float, currency: Currency):
         if not isinstance(currency, Currency):
             raise TypeError("currency should be an instance of Currency")
+        if amount < 0:
+            raise ValueError("amount should be positive")
         self.amount_dict[currency] = self.amount_dict[currency] + amount if currency in self.amount_dict else amount
--- python/tests/test_portfolio.py
+++ python/tests/test_portfolio.py
@@ -17,15 +17,38 @@ class TestPortfolio:
         portfolio.deposit(10, Currency.EUR)
         bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
         assert portfolio.evaluate(bank, Currency.USD) == 12
+
+    def test_should_deposit_an_amount_in_different_currencies(self):
+        portfolio: Portfolio = Portfolio()
+        portfolio.deposit(10, Currency.EUR)
+        portfolio.deposit(5, Currency.USD)
+        bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
+        assert portfolio.evaluate(bank, Currency.USD) == 17
     
     def test_should_deposit_the_same_currency(self):
         portfolio: Portfolio = Portfolio()
-        portfolio.deposit(10, Currency.EUR)
-        portfolio.deposit(20, Currency.EUR)
+        portfolio.deposit(10, Currency.USD)
+        portfolio.deposit(20, Currency.USD)
         bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
-        assert portfolio.evaluate(bank, Currency.EUR) == 30
+        assert portfolio.evaluate(bank, Currency.USD) == 30
 
     def test_shouldnot_add_an_unvalid_currency(self):
         portfolio: Portfolio = Portfolio()
         with pytest.raises(TypeError):
             portfolio.deposit(10, "Yen")
+
+    def test_shouldnot_convert_if_exchange_rate_is_missing(self):
+        portfolio: Portfolio = Portfolio()
+        portfolio.deposit(10, Currency.EUR)
+        bank: Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
+        with pytest.raises(Exception) as error:
+            portfolio.evaluate(bank, Currency.KRW)
+        
+        assert str(error.value) == "EUR->KRW"
+
+    def test_should_raise_error_when_depositing_negative_amout(self):
+        portfolio: Portfolio = Portfolio()
+        with pytest.raises(ValueError) as error:
+            portfolio.deposit(-1, Currency.EUR)
+        
+        assert str(error.value) == "amount should be positive"
