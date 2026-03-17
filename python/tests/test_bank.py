0802233299248b68021fde4958c6b8814f03a43e
ojfrancois <oceane.jean-francois@etu.u-bordeaux.fr>
2026-03-17 08:58:00 +0100

    modification des tests pour la banque avec money

--- python/tests/test_bank.py
+++ python/tests/test_bank.py
@@ -3,40 +3,40 @@ import pytest
 from xterm_craft_workshop.bank import Bank
 from xterm_craft_workshop.currency import Currency
 from xterm_craft_workshop.missing_exchange_rate_error import MissingExchangeRateError
+from xterm_craft_workshop.money import Money
 
 
 class TestBank:
-    
+
     bank : Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
-    
-    def test_should_convert_euro_to_usd_returns_float(self):
+
+    def test_should_convert_euro_to_usd_returns_money(self):
         bank : Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
-        
-        expected = 12
-        
-        converted = bank.convert(10, Currency.EUR, Currency.USD)
-        
+        expected = Money(12, Currency.USD)
+
+        converted = bank.convert(Money(10, Currency.EUR), Currency.USD)
+
         assert converted == expected
+        assert isinstance(converted, Money)
 
     def test_should_convert_euro_to_usd_returns_same_value(self):
-        expected = 10
-        
-        converted = self.bank.convert(10, Currency.EUR, Currency.EUR)
+        expected = Money(10, Currency.EUR)
+
+        converted = self.bank.convert(Money(10, Currency.EUR), Currency.EUR)
         assert converted == expected
 
     def test_should_convert_with_missing_exchange_rate_throws_exception(self):
         with pytest.raises(MissingExchangeRateError) as error:
-            self.bank.convert(10, Currency.EUR, Currency.KRW)
-        
+            self.bank.convert(Money(10, Currency.EUR), Currency.KRW)
+
         assert str(error.value) == "EUR->KRW"
 
     def test_should_convert_with_different_exchange_rate_returns_different_floats(self):
         bank : Bank = Bank.create(Currency.EUR, Currency.USD, 1.2)
         epsilon = 0.001
-        
-        converted1 = bank.convert(10, Currency.EUR, Currency.USD)
+
+        converted1 = bank.convert(Money(10, Currency.EUR), Currency.USD)
         self.bank.addEchangeRate(Currency.EUR, Currency.USD, 1.3)
-        converted2 = bank.convert(10, Currency.EUR, Currency.USD)
-        
-        assert converted1 - converted2 < epsilon
-        
+        converted2 = bank.convert(Money(10, Currency.EUR), Currency.USD)
+
+        assert abs(converted1.amount - converted2.amount) < epsilon
