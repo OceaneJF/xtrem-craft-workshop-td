72b41863f7f101f6310f2735a98f3a616c8e184a
ojfrancois <oceane.jean-francois@etu.u-bordeaux.fr>
2026-03-19 09:41:33 +0100

    implémentation de BankBuilder

---
+++ python/tests/bank_builder.py
@@ -0,0 +1,17 @@
+from xterm_craft_workshop.bank import Bank
+from xterm_craft_workshop.currency import Currency
+
+
+class BankBuilder:
+    def __init__(self):
+        self._rates: list[tuple[Currency, float]] = []
+
+    def with_exchange_rate(self, currency: Currency, rate: float) -> "BankBuilder":
+        self._rates.append((currency, rate))
+        return self
+
+    def build(self) -> Bank:
+        bank = Bank({})
+        for currency, rate in self._rates:
+            bank.addEchangeRate(Currency.EUR, currency, rate)
+        return bank
