2c4e6698307e18fa45cb50185c39c17a2ad490c2
ojfrancois <oceane.jean-francois@etu.u-bordeaux.fr>
2026-03-19 11:19:11 +0100

    fin des tests pour bank

--- python/src/xterm_craft_workshop/bank.py
+++ python/src/xterm_craft_workshop/bank.py
@@ -2,25 +2,27 @@ from typing import Dict
 from xterm_craft_workshop.money import Money
 from xterm_craft_workshop.currency import Currency
 from xterm_craft_workshop.missing_exchange_rate_error import MissingExchangeRateError
+from xterm_craft_workshop.exchange_rate import Exchange_rate
 
 
 class Bank:
 
-    def __init__(self, exchange_rate: Dict[str, float] = {}) -> None:
+    def __init__(self, pivot : Currency, exchange_rate: list[Exchange_rate] = []) -> None:
         self._exchange_rate = exchange_rate
+        self._pivot = pivot
 
     @staticmethod
-    def create(from_currency: Currency, to_currency: Currency, rate: float) -> "Bank":
-        bank = Bank({})
-        bank.addEchangeRate(from_currency, to_currency, rate)
+    def create(exchange_rate : Exchange_rate, pivot : Currency) -> "Bank":
+        bank = Bank(pivot)
+        bank.addEchangeRate(exchange_rate)
 
         return bank
     
-    def addEchangeRate(self, from_currency: Currency, to_currency: Currency, rate: float) -> None:
-        self._exchange_rate[f'{from_currency.value}->{to_currency.value}'] = rate
+    def addEchangeRate(self, exchange_rate: Exchange_rate) -> None:
+        self._exchange_rate.append(exchange_rate)
 
-    def can_convert(self, from_currency: Currency, to_currency: Currency) -> bool:
-        return from_currency.value == to_currency.value or f'{from_currency.value}->{to_currency.value}' in self._exchange_rate
+    def can_convert(self, currency: Currency) -> bool:
+        return any(er.currency == currency for er in self._exchange_rate)
     
     def convert(self, money: Money, to_currency: Currency) -> Money:
         if not self.can_convert(money.currency, to_currency):
---
+++ python/src/xterm_craft_workshop/exchange_rate.py
@@ -0,0 +1,5 @@
+class ExchangeRate:
+    def __init__(self, currency: str, rate: float):
+        self.currency = currency
+        self.rate = rate
+
